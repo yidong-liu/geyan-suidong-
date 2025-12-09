48453328c98c7f85b599729d75650b27e3710f02
刘易东 <154659668+yidong-liu@users.noreply.github.com>
2025-12-09 10:21:04 +0000

    feat: 【live2d】修改请求ip和触发方式

--- plug/CubismSdkForWeb-5-r.1/Samples/TypeScript/Demo/src/lappselectionmonitor.ts
+++ plug/CubismSdkForWeb-5-r.1/Samples/TypeScript/Demo/src/lappselectionmonitor.ts
@@ -124,10 +124,10 @@ export class SelectionMonitor {
         this._expressionManager.setExpression(expression);
       }
     });
-    if (event.ctrlKey && event.key === "0") {
+    if (event.ctrlKey && event.key === "w") {
       // 获取一个表情数组 localhost: 8000
       axios
-        .post("http://localhost:8000/getNeutralExpression", {})
+        .post("http://localhost:8000/api/v1/getNeutralExpression", {})
         .then((response: AxiosResponse) => {
           const neutralExpression = response.data.neutralExpression;
           this._expressionManager.setExpressions(neutralExpression);
